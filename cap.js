26cacf38e1eeabd9254bffd491378c41269f3f56
Ali <mitali777770@gmail.com>
2026-04-30 00:10:15 +0900

    feat: advantages of callback and promise

---
+++ cap.js
@@ -0,0 +1,76 @@
+/* Asynchronous function:callback vs Async vs Promise
+
+DEFINE       CALL
+callback     callback
+async/await  then/catch
+promise      then/catch
+
+*/
+/*
+//define
+function division(a, b, callback) {
+    if(b == 0) {
+        callback("Not divided by zero, you cannot divide number to 0", null)
+    } else {
+        callback(null, a % b)
+    }
+}
+
+//call 
+division(10,3, function(err, data) {
+    if(err) console.log("ERROR:", err)
+        else {
+            // setTimeout(function() {
+            //     console.log("RESULT:",data)
+            //     console.log("...")
+            // }, 2000)}
+            setInterval(function() {
+                console.log("RESULT:",data)
+                console.log("...")
+            }, 2000)}
+});
+ */
+/*
+
+//define
+async function division(a, b) {
+    if(b == 0) {
+        throw new Error("Not divided by zero")
+    } else {
+        // setTimeout(function() async larda setTimeout, setInterval ishlamaydi
+        // }, 2000)
+        return a % b
+    }
+}
+
+//call 
+division(10, 3).then(data => {
+    console.log("Result:", data)
+    console.log(".......");
+    
+}).catch(err => {
+    console.log("Error division:",err)
+}); */
+
+
+//define
+function division(a, b) {
+    return new Promise((resolve, reject) => {
+        if(b === 0) {
+            reject("Not divided by zero")
+        } else {
+            setTimeout(function() { //setInterval ishlaydi lekin bir marta takrorlanadi
+                resolve(a % b);
+            }, 2000)
+        }
+    })
+}
+
+//call 
+division(10, 3).then(data => {
+    console.log("Result:", data)
+    console.log(".......");
+    
+}).catch(err => {
+    console.log("Error division:",err)
+});
--- server.js
+++ server.js
@@ -3,19 +3,19 @@
 
 const express = require('express')
 const app = express()
-// const port = 3000
+const port = 3555
 
 
 // Traditional Get Api
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Hello World!</h1>')
 })
 // Rest Api
 app.get('/rest', (req, res) => {
-  res.json({name: "Marco", age: 21, group: "MIT"})
+    res.json({name: "Marco", age: 21, group: "MIT"})
 })
 
 
-app.listen(3000, () => {
-  console.log(`Backend Server is running on port 3000`)
+app.listen(port, () => {
+  console.log(`Backend Server is running on port ${port}`)
 })
