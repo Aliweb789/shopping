928732eae0ca7190b678ea5b70a6de508face80c
Ali <mitali777770@gmail.com>
2026-04-30 00:23:11 +0900

    fix: advantages of async/await funct

--- cap.js
+++ cap.js
@@ -2,7 +2,7 @@
 
 DEFINE       CALL
 callback     callback
-async/await  then/catch
+async/await  then/catch async/await
 promise      then/catch
 
 */
@@ -53,43 +53,24 @@ division(10, 3).then(data => {
 }); */
 
 //define
-function division(a, b) {
-  return new Promise((resolve, reject) => {
+async function division(a, b) {
     if (b === 0) {
-      reject("Not divided by zero");
+      throw new Error("Not divided by zero");
     } else {
-      setTimeout(function () {
-        //setInterval ishlaydi lekin bir marta takrorlanadi
-        resolve(a % b);
-      }, 2000);
+        return a % b;
     }
-  });
 }
 
-//call
-division(10, 3)
-  .then((data) => {
-    console.log("Result:", data);
-    console.log(".......");
 
-    division(10, 3)
-      .then((data) => {
-        console.log("Result:", data);
-        console.log(".......");
+async function run() {
+    let result  = await division(10, 3);
+    console.log("result one:", result);
 
-        division(10, 3)
-          .then((data) => {
-            console.log("Result:", data);
-            console.log(".......");
-          })
-          .catch((err) => {
-            console.log("Error division:", err);
-          });
-      })
-      .catch((err) => {
-        console.log("Error division:", err);
-      });
-  })
-  .catch((err) => {
-    console.log("Error division:", err);
-  });
+    result  = await division(10, 4);
+    console.log("result two:", result);
+    
+    result  = await division(20, 7);
+    console.log("result three:", result);
+}
+
+run()
