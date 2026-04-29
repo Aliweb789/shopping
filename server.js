c4149f1e92114a153215cdede35e99bbbcc924a1
Ali <mitali777770@gmail.com>
2026-04-29 20:15:51 +0900

    feat: building simple backend server on nodejs

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 // NodeJS EXPRESS NESTJS
 // Python DGANJO FastApi
+
+const express = require('express')
+const app = express()
+// const port = 3000
+
+
+// Traditional Get Api
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+// Rest Api
+app.get('/rest', (req, res) => {
+  res.json({name: "Marco", age: 21, group: "MIT"})
+})
+
+
+app.listen(3000, () => {
+  console.log(`Backend Server is running on port 3000`)
+})
